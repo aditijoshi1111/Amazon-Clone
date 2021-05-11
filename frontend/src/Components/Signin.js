30e1db71b3f4600fb9dbaa57e6a4c7f298535b24
anshika00 <anshika1242.cse18@chitkara.edu.in>
2021-05-11 10:57:51 +0530

    signin

--- frontend/src/Components/Signin.js
+++ frontend/src/Components/Signin.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import CSSlogin from '../CSSstyles/Signin.module.css';
+import logo from '../img/logo.png'
 
 export const Signin = () => {
     return (
         <div className="signin">
            <Link to="/">
                 <img
-                className={CSSNav.logo} 
+                className={CSSlogin.logo} 
                 src={logo} 
                 alt="qwerty"/>
                 </Link>
