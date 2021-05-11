17237c058e8bb1a83b1ded44a0f58ceeefb9fbf5
pallavi <pallavigrover210@gmail.com>
2021-05-11 17:50:58 +0530

    commit ur

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,9 @@
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-<<<<<<< HEAD
-import Navbar from './Components/Navbar';
-import Footer from './Components/Footer';
-
-function App() {
-  return (
-    <>
-  <BrowserRouter>
-    <div className="App">
-      <Switch>
-        <Route exact to="/">
-          <Navbar></Navbar>
-        </Route>
-
-      </Switch>
-      <Footer></Footer>
-      
-    </div>
-  </BrowserRouter>
-  
-  
-  
-  </>
-=======
 import Navbar from "./Components/Navbar";
 import SignIn from "./Components/Signin";
 import Home from "./Components/Home";
-
+import Footer from "./Components/Footer"
 function App() {
   return (
     <BrowserRouter>
@@ -43,9 +19,9 @@ function App() {
             <SignIn />
           </Route>
         </Switch>
+        <Footer></Footer>
       </div>
     </BrowserRouter>
->>>>>>> db532f3016045272175d4f07e35ffd98194646a3
   );
 }
 
