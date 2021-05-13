8818343c1bd2bf249b49b5caed772eaba3bb3187
pallavi <pallavigrover210@gmail.com>
2021-05-13 19:35:37 +0530

    palavi

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,7 +26,7 @@ function App() {
             <ContactUs/>
           </Route>
           <Route path="/cart">
-            <Checkout />
+            <Checkout/>
           </Route>
         </Switch>
         <Footer></Footer>
