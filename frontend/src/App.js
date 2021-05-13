669fe9b21651ad30be14241475dac3f4a93aa40c
anshika00 <anshika1242.cse18@chitkara.edu.in>
2021-05-13 10:50:18 +0530

    error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,9 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import SignIn from "./Components/Signin";
 import Home from "./Components/Home";
-<<<<<<< HEAD
-import Footer from "./Components/Footer"
-import { MyCard } from "./Components/MyCard";
-=======
 import Footer from "./Components/Footer";
 import Checkout from "./Components/Checkout";
 import ContactUs from './Components/ContactUs';
->>>>>>> dcd24ac2814500f55c26f4575bf64d61cb71b17b
 
 function App() {
   return (
