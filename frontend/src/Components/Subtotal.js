8f07ba9607f5e2e6299e51e60c3d2ca2dde1cfd5
anshika00 <anshika1242.cse18@chitkara.edu.in>
2021-05-13 19:00:49 +0530

    merge

--- frontend/src/Components/Subtotal.js
+++ frontend/src/Components/Subtotal.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-/*import React from 'react';
-import SubtotalCss from '../CSSstyles/Subtotal.module.css';
-import CurrencyFormat from 'react-currency-format';
-
-function Subtotal() {
-    return (
-        <div className={SubtotalCss.subtotal1}>
-            <CurrencyFormat
-                renderText={(value) => (
-                    <>
-                        <p>
-                            <label>Subtotal (0 items) : </label><strong>0</strong>
-                        </p>
-
-                        <small className={SubtotalCss.subtotal_gift}>
-                            <input type="checkbox" />This order conatins a gift
-               </small>
-                    </>
-                )}
-                decimalScale={2}
-                value={0}
-                displayType={"text"}
-                thousandSeparator={true}
-                prefix={"$"}
-            />
-            <br />
-            <br />
-            <button>Proceed To checkout</button>
-        </div>
-    )
-}
-export default Subtotal;*/
-=======
  import React from 'react';
  import SubtotalCss from '../CSSstyles/Subtotal.module.css';
  import CurrencyFormat from 'react-currency-format';
@@ -64,4 +30,3 @@ export default Subtotal;*/
     )
  }
  export default Subtotal;
->>>>>>> anshika_branch
