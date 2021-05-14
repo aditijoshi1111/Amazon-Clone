2409a800261558b692d2a10af23d6e5b41aa7219
pallavi <pallavigrover210@gmail.com>
2021-05-14 17:58:55 +0530

    count

--- frontend/src/Components/Subtotal.js
+++ frontend/src/Components/Subtotal.js
@@ -1,56 +1,26 @@
-<<<<<<< HEAD
- import React from 'react';
-=======
 import React from 'react';
->>>>>>> Pallavi_branch
  import SubtotalCss from '../CSSstyles/Subtotal.module.css';
  import CurrencyFormat from 'react-currency-format';
 
  function Subtotal () {
      return (
          <div className={SubtotalCss.subtotal1}>
-<<<<<<< HEAD
-         <CurrencyFormat
-           renderText = {(value) =>(
-                <>
-                <p>
-                    <label>Subtotal (0 items) : </label><strong>0</strong>
-                </p>
-=======
        <CurrencyFormat
            renderText = {(value) =>(
                <>
                <p>
                     <label>Subtotal (0 items) : </label><strong>0</strong>
                </p>
->>>>>>> Pallavi_branch
               
                  <small className={SubtotalCss.subtotal_gift}>
                    <input type="checkbox"/>This order conatins a gift
-<<<<<<< HEAD
-                </small>
-                </>
-            )}
-=======
               </small>
                </>
            )}
->>>>>>> Pallavi_branch
            decimalScale={2}
             value={0}
             displayType={"text"}
             thousandSeparator={true}
-<<<<<<< HEAD
-            prefix={"$"}
-            />
-           <br/>
-           <br/>
-        <button>Proceed To checkout</button>   
-         </div>
-    )
- }
- export default Subtotal;
-=======
            prefix={"$"}
            />
            <br/>
@@ -60,4 +30,3 @@ import React from 'react';
      )
  }
  export default Subtotal;
->>>>>>> Pallavi_branch
