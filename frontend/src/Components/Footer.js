7fbb12dcee358ff9eb6cfc7fdb8fd36a9a3eb520
aditijoshi1111 <aditi1111.cse18@chitkara.edu.in>
2021-05-20 18:37:46 +0530

    css

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -9,17 +9,16 @@ import MailIcon from '@material-ui/icons/Mail';
 import CSSFooter from "../CSSstyles/Footer.module.css";
 export default function Footer() {
     
-    
     return (
         <div className={CSSFooter.container}>
                 <div className={CSSFooter.row}>
                     <div className="col-md-3 col-sm-6">
-                
+
                         <ul className={CSSFooter.list}>
-                        <Link to="" > <li>SEARCH OUR STORE</li></Link>
-                        <Link to="" > <li>BLOG</li></Link>
-                        <Link to="" > <li>ABOUT US</li></Link>
-                        <Link to="/contactUs" > <li>CONTACT US</li></Link>
+                        <h4>Get to know Us</h4>
+                        <Link to="" > <li>Blog</li></Link>
+                        <Link to="/aboutUs" > <li>About Us</li></Link>
+                        <Link to="/contactUs" > <li>Contact Us</li></Link>
                         </ul>
                     </div>
     
@@ -32,13 +31,12 @@ export default function Footer() {
                     </div>
                     
                     <div className="col">
-                        
                     <p> 
-                <br></br><br></br><br></br>
-                      <Link to="" style={{padding: "5px", textDecoration: 'none' ,color:'white'}}>   <GitHubIcon/></Link>     
-                      <Link to="" style={{ padding: "5px", textDecoration: 'none' ,color:'white'}}><InstagramIcon></InstagramIcon></Link>
-                      <Link to="" style={{ padding: "5px", textDecoration: 'none' ,color:'white'}}>   <FacebookIcon/></Link> 
-                      <Link to="" style={{ padding: "5px", textDecoration: 'none' ,color:'white'}}> <TwitterIcon/></Link>  
+                    <br></br><br></br><br></br>
+                      <Link to="" ><GitHubIcon className={CSSFooter.social}/></Link>     
+                      <Link to="" ><InstagramIcon className={CSSFooter.social}/></Link>
+                      <Link to="" ><FacebookIcon className={CSSFooter.social}/></Link> 
+                      <Link to="" ><TwitterIcon className={CSSFooter.social}/></Link>  
                     </p>
                     </div>
                         
