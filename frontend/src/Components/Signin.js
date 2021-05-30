a0d707cf9f5df2c92e006fe85c82c95f90f93f13
KingshuK Choudhary <53696144+kingshuk441@users.noreply.github.com>
2021-05-30 21:55:05 +0530

    redirect

--- frontend/src/Components/Signin.js
+++ frontend/src/Components/Signin.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, Redirect, useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { authenticate, isAutheticated, signin } from "../apis/auth";
 import { getOrders } from "../apis/order";
 import CSSlogin from "../CSSstyles/Signin.module.css";
